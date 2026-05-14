512f26edccd8ca8edcb5baf508d8635ec550415d
alexandercao22 <144004304+alexandercao22@users.noreply.github.com>
2026-05-14 15:05:22 +0200

    Created cube as a struct

--- src/Data.h
+++ src/Data.h
@@ -1,4 +1,34 @@
+#pragma once
+
 #include <DirectXMath.h>
+
+struct VertexPosColor
+{
+	DirectX::XMFLOAT3 position;
+	DirectX::XMFLOAT3 color;
+};
+
+struct Cube {
+	VertexPosColor vertices[8] = {
+		{ DirectX::XMFLOAT3(-1.0f, -1.0f, -1.0f), DirectX::XMFLOAT3(0.0f, 0.0f, 0.0f) }, // 0
+		{ DirectX::XMFLOAT3(-1.0f,  1.0f, -1.0f), DirectX::XMFLOAT3(0.0f, 1.0f, 0.0f) }, // 1
+		{ DirectX::XMFLOAT3(1.0f,  1.0f, -1.0f), DirectX::XMFLOAT3(1.0f, 1.0f, 0.0f) }, // 2
+		{ DirectX::XMFLOAT3(1.0f, -1.0f, -1.0f), DirectX::XMFLOAT3(1.0f, 0.0f, 0.0f) }, // 3
+		{ DirectX::XMFLOAT3(-1.0f, -1.0f,  1.0f), DirectX::XMFLOAT3(0.0f, 0.0f, 1.0f) }, // 4
+		{ DirectX::XMFLOAT3(-1.0f,  1.0f,  1.0f), DirectX::XMFLOAT3(0.0f, 1.0f, 1.0f) }, // 5
+		{ DirectX::XMFLOAT3(1.0f,  1.0f,  1.0f), DirectX::XMFLOAT3(1.0f, 1.0f, 1.0f) }, // 6
+		{ DirectX::XMFLOAT3(1.0f, -1.0f,  1.0f), DirectX::XMFLOAT3(1.0f, 0.0f, 1.0f) }  // 7
+	};
+	unsigned short indices[36] = {
+		0, 1, 2, 0, 2, 3,
+		4, 6, 5, 4, 7, 6,
+		4, 5, 1, 4, 1, 0,
+		3, 2, 6, 3, 6, 7,
+		1, 5, 6, 1, 6, 2,
+		4, 0, 3, 4, 3, 7
+	};
+	DirectX::XMMATRIX matrix = DirectX::XMMatrixIdentity();
+};
 
 struct Cbuffer {
 	DirectX::XMMATRIX MVP;
