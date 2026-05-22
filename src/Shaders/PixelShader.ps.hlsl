5558b3400291147b97c0307e9eb6dabb0a7ae1c5
alexandercao22 <144004304+alexandercao22@users.noreply.github.com>
2026-05-22 19:21:03 +0200

    Removed temporary CBVs from geometry pixel shader

--- src/Shaders/PixelShader.ps.hlsl
+++ src/Shaders/PixelShader.ps.hlsl
@@ -1,20 +1,3 @@
-struct Camera
-{
-    float4 camPos;
-    matrix viewProj;
-};
-
-struct RayData
-{
-    uint totalSpotLights;
-    uint totalPointLights;
-    uint frameCount;
-    float pad;
-};
-
-ConstantBuffer<Camera> cameraCB : register(b0);
-ConstantBuffer<RayData> rayDataCB : register(b1);
-
 struct PixelShaderInput
 {
 	float4 Color    : COLOR;
@@ -22,6 +5,5 @@ struct PixelShaderInput
 
 float4 main( PixelShaderInput IN ) : SV_Target
 {
-    return IN.Color * cameraCB.camPos * rayDataCB.frameCount;
     return IN.Color;
 }
