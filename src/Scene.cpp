007842a51765ce43c67325f067b037c51dc7b263
alexandercao22 <144004304+alexandercao22@users.noreply.github.com>
2026-05-25 14:01:00 +0200

    Added simple camera movement

--- src/Scene.cpp
+++ src/Scene.cpp
@@ -4,9 +4,11 @@ void Scene::Initialize()
 {
 	// Camera
 	{
-		DirectX::XMFLOAT4 position = { 0.0f, 0.0f, -10.0f, 0.0f };
+		m_position = { 0.0f, 0.0f, -10.0f };
+		m_forward = { 0.0f, 0.0f, 1.0f };
+
 		// Create the view matrix.
-		const DirectX::XMVECTOR eyePosition = DirectX::XMLoadFloat4(&position);
+		const DirectX::XMVECTOR eyePosition = DirectX::XMLoadFloat3(&m_position);
 		const DirectX::XMVECTOR focusPoint = DirectX::XMVectorSet(0, 0, 0, 1);
 		const DirectX::XMVECTOR upDirection = DirectX::XMVectorSet(0, 1, 0, 0);
 		DirectX::XMMATRIX view = DirectX::XMMatrixLookAtLH(eyePosition, focusPoint, upDirection);
@@ -63,6 +65,44 @@ void Scene::Update(float deltaTime, float runTime)
 	DirectX::XMMATRIX modelMatrix = DirectX::XMMatrixRotationAxis(rotationAxis, DirectX::XMConvertToRadians(angle));
 	modelMatrix = DirectX::XMMatrixTranspose(modelMatrix);
 	DirectX::XMStoreFloat4x4(&m_cubeInstances[0].model, modelMatrix);
+
+	if (GetAsyncKeyState('W') & 0x8000) {
+		m_position.z += m_moveSpeed * deltaTime;
+	}
+	if (GetAsyncKeyState('S') & 0x8000) {
+		m_position.z -= m_moveSpeed * deltaTime;
+	}
+	if (GetAsyncKeyState('D') & 0x8000) {
+		m_position.x += m_moveSpeed * deltaTime;
+	}
+	if (GetAsyncKeyState('A') & 0x8000) {
+		m_position.x -= m_moveSpeed * deltaTime;
+	}
+	if (GetAsyncKeyState(VK_SPACE) & 0x8000) {
+		m_position.y += m_moveSpeed * deltaTime;
+	}
+	if (GetAsyncKeyState(VK_LSHIFT) & 0x8000) {
+		m_position.y -= m_moveSpeed * deltaTime;
+	}
+
+
+	// Camera
+	{
+		// Create the view matrix.
+		const DirectX::XMVECTOR eyePosition = DirectX::XMLoadFloat3(&m_position);
+		const DirectX::XMVECTOR focusPoint = DirectX::XMVectorAdd(DirectX::XMLoadFloat3(&m_position), DirectX::XMLoadFloat3(&m_forward));
+		const DirectX::XMVECTOR upDirection = DirectX::XMVectorSet(0, 1, 0, 0);
+		DirectX::XMMATRIX view = DirectX::XMMatrixLookAtLH(eyePosition, focusPoint, upDirection);
+		view = DirectX::XMMatrixTranspose(view);
+		DirectX::XMStoreFloat4x4(&m_camera.view, view);
+
+		// Create the projection matrix.
+		float fov = 45.0f;
+		float aspectRatio = 1280 / static_cast<float>(720);
+		DirectX::XMMATRIX proj = DirectX::XMMatrixPerspectiveFovLH(DirectX::XMConvertToRadians(fov), aspectRatio, 0.1f, 100.0f);
+		proj = DirectX::XMMatrixTranspose(proj);
+		DirectX::XMStoreFloat4x4(&m_camera.proj, proj);
+	}
 }
 
 const PerFrame &Scene::GetCamera() const
--- src/Scene.h
+++ src/Scene.h
@@ -21,7 +21,11 @@ public:
 	DirectionalLight GetDirectionlLight() const;
 
 private:
+	DirectX::XMFLOAT3 m_position;
+	DirectX::XMFLOAT3 m_forward;
 	PerFrame m_camera;
+
+	float m_moveSpeed = 10.0f;
 
 	std::vector<PerObject> m_cubeInstances;
 
